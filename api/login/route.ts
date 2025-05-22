286e01d5296ea20e0260866f65966d7beaafdc5c
Christian Duong <christian.duong2@education.nsw.gov.au>
2025-05-22 16:41:56 +1000

    Update route.ts

--- api/login/route.ts
+++ api/login/route.ts
@@ -1,35 +1,34 @@
 import { NextRequest, NextResponse } from 'next/server';
-import * as mongoDB from "mongodb";
+import clientPromise from "../connection/mongodb";
 
 export async function POST(req: NextRequest) {
-    const body = await req.json();
-    const { email, password, is_manager } = body;
+  const body = await req.json();
+  const { email, password } = body;
 
-    try {
-        const client = new mongoDB.MongoClient("mongodb+srv://christian:3eLaMQ6N4z51Riza@rostersystem.xg9w7kd.mongodb.net/?retryWrites=true&w=majority&appName=RosterSystem");
-        await client.connect();
-        const db = client.db("EmployeeInfo");
-        const collection = db.collection("Employees");
+  try {
+    const client = await clientPromise;
+    const db = client.db("EmployeeInfo");
+    const collection = db.collection("Employees");
 
-        const user = await collection.findOne({
-            email: email,
-            password: password,
-            is_manager: is_manager
-        })
+    const user = await collection.findOne({ email, password });
 
-        await client.close();
-
-        if (user) {
-            return NextResponse.json({ success: true, message: "Login successful" });
-        } else {
-            return NextResponse.json (
-                { success: false, message: "Invalid password, email or invalid role"},
-                { status: 401}
-            );
-        }
-    } catch (error) {
-        console.error(error);
-        return NextResponse.json({ success: false, message: (error as Error).message }, 
-        { status: 500 });
+    if (user) {
+      return NextResponse.json({
+        success: true,
+        message: "Login successful",
+        is_manager: user.is_manager
+      });
+    } else {
+      return NextResponse.json(
+        { success: false, message: "Invalid email or password" },
+        { status: 401 }
+      );
     }
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { success: false, message: (error as Error).message },
+      { status: 500 }
+    );
+  }
 }
